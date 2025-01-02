e546cb6087592bb10fa7eb446cc6dd06a5ea8eb4
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2025-01-02 15:03:33 +0000

    I created characters for a game using dictionaries and giving them different colours based on their types

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import time,os
+blue = '\033[34m'
+red = '\033[31m'
+green = '\033[32m'
+default = '\033[0m'
+yellow = '\033[33m'
 
+print (f"{yellow}MokéBeast{default}")
+
+myBeast = {"Beast Name" : None, "Type" : None, "Special move": None, "HP": None, "MP" : None}
+print( )
+
+for name, value in myBeast.items( ):
+  myBeast[name] = input(f"{name}: ").strip( ).title( )
+  print( )
+time.sleep(1)
+os.system("clear")
+
+if myBeast ["Type"] =="Air":
+     print(f"{blue}",end = " ")
+elif myBeast["Type"]  == "Fire":
+     print(f"{red}",end = " " )
+elif myBeast ["Type"] == "Earth":
+     print(f"{green}",end = " ")
+elif myBeast ["Type"] == "Water":
+     print(f"{default}",end = " ")
+else:
+     print(f"{yellow}",end = " ")
+for name , value in myBeast.items( ):
+      print (f"{name} : {value} ")
+      print()
